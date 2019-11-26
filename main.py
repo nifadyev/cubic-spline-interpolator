d81787051a4410bf665bc198c070ee9e365b65f7
Vadim Nifadyev <vnifadyev@gmail.com>
2019-11-26 22:14:57 +0300

    Add new cli arguments, move logic inside class

--- main.py
+++ main.py
@@ -12,90 +12,53 @@ def function(argument):
     """Find function's result with specified argument."""
     return argument * np.sin(argument) / (1 + argument**2)
 
+
 @click.command()
 @click.option(
     '--range_start', type=float, default=-10.0, help='Left range boundary')
 @click.option(
     '--range_end', type=float, default=10.0, help='Right range boundary')
 @click.option(
-    '--step', type=int, default=10, help='Step between values in range')
+    '--range_length', type=int, default=30000, help='Number of values in range')
+@click.option(
+    '--intervals', type=int, default=10, help='Number of invervals')
 @click.option(
     '--compare_default/--do_not_compare', default=False,
     help='Compare custom spline with default scipy spline'
 )
-def main(range_start, range_end, step, compare_default):
-    spline = CubicSplineInterpolator(range_start, range_end, 0.0, step, function)
-    x = spline.args
-    y = spline.results
+def main(range_start, range_end, range_length, intervals, compare_default):
+    epsilon = (range_end - range_start) / range_length
+    spline = CubicSplineInterpolator(
+        range_start, range_end, epsilon, intervals, function)
+    arguments = spline.args
+    interpolated_results = spline.results
 
-    def_y = [function(i) for i in x]
+    function_results = [function(i) for i in arguments]
+
+    spline.print_calculations()
 
     plt.figure('Interpolator')
-    plt.title('Cubic spline interpolation with tridiagonal matrix algorithm')
+    plt.title('Function: x * sin(x) / (1 + x * x)')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.grid(True)
-    plt.plot(x, def_y, label='function')
-    plt.plot(x, y, label='spline')
+    plt.plot(arguments, function_results, label='function')
+    plt.plot(arguments, interpolated_results, label='spline')
+
+    if compare_default:
+        scipy_spline = CubicSpline(arguments, interpolated_results)
+        plt.figure('Scipy spline interpolator')
+        plt.plot(arguments, function_results, label='function')
+        plt.plot(arguments, scipy_spline(arguments), label="S")
+        # plt.plot(
+        #     arguments, scipy_spline(arguments, 1), label="S'")
+        # plt.plot(
+        #     spline_arguments, scipy_spline(spline_arguments, 2), label="S''")
+        # plt.plot(
+        #     spline_arguments, scipy_spline(spline_arguments, 3), label="S'''")
+
     plt.legend()
     plt.show()
-
-
-
-# @click.command()
-# @click.option(
-#     '--range_start', type=float, default=0.0, help='Left range boundary')
-# @click.option(
-#     '--range_end', type=float, default=10.0, help='Right range boundary')
-# @click.option(
-#     '--step', type=float, default=0.25, help='Step between values in range')
-# @click.option(
-#     '--compare_default/--do_not_compare', default=False,
-#     help='Compare custom spline with default scipy spline'
-# )
-# def main(range_start, range_end, step, compare_default):
-#     """Build spline and interpolate it's values.
-
-#     Set function and it's function_arguments and use CubicSplineInterpolator
-#     to interpolate spline in specified function_arguments.
-#     """
-#     function_arguments = np.linspace(
-#         range_start, range_end, (range_end - range_start) / 0.25)
-#     spline_arguments = np.linspace(
-#         range_start, range_end, (range_end - range_start) / step)
-#     results = [function(arg) for arg in function_arguments]
-
-#     spline = CubicSplineInterpolator(
-#         function_arguments, results, steps)
-#     # spline = CubicSplineInterpolator(
-#     #     function_arguments, results, spline_arguments)
-
-#     print('Function: x * sin(x) / (1 + x * x)\n')
-#     spline.print_calculations(function)
-
-#     plt.figure('Interpolator')
-#     plt.title('Cubic spline interpolation with tridiagonal matrix algorithm')
-#     plt.xlabel('x')
-#     plt.ylabel('y')
-#     plt.grid(True)
-#     plt.plot(function_arguments, results, label='function')
-#     plt.plot(spline_arguments, spline.interpolated_data, label='spline')
-
-#     if compare_default:
-#         scipy_spline = CubicSpline(function_arguments, results)
-#         plt.figure('Scipy spline interpolator')
-#         # plt.plot(function_arguments, results, 'o', label='data')
-#         plt.plot(function_arguments, results, label='function')
-#         plt.plot(spline_arguments, scipy_spline(spline_arguments), label="S")
-#         plt.plot(
-#             spline_arguments, scipy_spline(spline_arguments, 1), label="S'")
-#         # plt.plot(
-#         #     spline_arguments, scipy_spline(spline_arguments, 2), label="S''")
-#         # plt.plot(
-#             # spline_arguments, scipy_spline(spline_arguments, 3), label="S'''")
-
-#     plt.legend()
-#     plt.show()
 
 
 if __name__ == '__main__':
