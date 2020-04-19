4dd7208fd1042c77e1d49bb989db34a18bd7fe49
Vadim Nifadyev <vnifadyev@gmail.com>
2020-04-19 11:54:10 +0300

    Simplify test

--- tests/test_interpolator.py
+++ tests/test_interpolator.py
@@ -1,17 +1,11 @@
-import pytest
-
 import src.interpolator as intr
 import numpy as np
 
 
-def function(argument):
-    """Find function's result with specified argument."""
-    return argument * np.sin(argument) / (1 + argument**2)
+def test_get_interpolation_error():
+    """Check if interpolation error is calculated as expected."""
+    epsilon = (10.0 + 10.0) / 30000
+    interpolator = intr.CubicSplineInterpolator(
+        -10.0, 10.0, epsilon, 10, lambda arg: arg * np.sin(arg) / (1 + arg**2))
 
-class TestCubicSplineInterpolator():
-
-    def test_get_interpolation_error(self):
-        epsilon = (10.0 + 10.0) / 30000
-        interpolator = intr.CubicSplineInterpolator(-10.0, 10.0, epsilon, 10, function)
-
-        assert interpolator.get_interpolation_error() == 0.11197798190992969
+    assert interpolator.get_interpolation_error() == 0.11197798190992969
